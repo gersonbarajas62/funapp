4885cc19fa44bf358717c879b97ad16ec27d71f0
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-04-06 16:55:38 -0600

    dependencies

---
+++ public/react/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';}
+import ReactDom from 'react-dom';
+import $ from 'jquery';
+
+
+ class App extends React.Component{
+
+
+   render(){
+     return(
+       <h1>fetus</h1>
+     )
+   }
+ }
+
+
+ReactDom.render(<App />,document.getElementById('app'));
